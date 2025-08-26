091498a6459de16590a542fbd52ae54b3027d920
Kyle Frederick <frederickkd@gmail.com>
2025-08-26 02:50:18 -0400

    Initial Landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://kdfreder.github.io',
   base: '/',
   outDir: 'dist',
+  integrations: [tailwind()],
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '.75rem',
+          lg: '1.5rem',
+          xl: '2rem',
+        },
+      },
+      colors: {
+        primary: '#3B82F6',
+        secondary: '#F3F4F6',
+        'white': '#FFFFFF',
+        'black': '#000000',
+        'dark': '#262626',
+        'body': '#333333',
+        'gray': '#999999',
+      },
+      fontFamily: {
+        body: ['inter', 'sans-serif'],
+        heading: ['Space Grotesk', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        bold: 700,
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            'body': {
+              'font-family': 'var(--font-body)',
+            },
+          },
+        },
+      },
+      boxShadow: {
+        'sm': '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+        'md': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        'lg': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        'xl': '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 8px 10px -6px rgba(0, 0, 0, 0.1)',
+        '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      },
+    },
+  },
+  plugins: [],
+}
