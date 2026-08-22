f9d92ea0984c81efaf13eacd7de3fe8ce8e476e9
Kyle Frederick <frederickkd@gmail.com>
2026-08-22 17:58:39 -0400

    Stack updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://kdfreder.github.io',
   base: '/',
   outDir: 'dist',
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   devToolbar: {
     enabled: false,
   },
